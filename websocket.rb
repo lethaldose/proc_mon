9131b434d9d1b32085b40f7a0585f7684e2064b2
Atul Verma <atulverma82@yahoo.com>
2012-10-16 00:56:03 +0530

    extracting proc info table in a partial

--- websocket.rb
+++ websocket.rb
@@ -4,7 +4,7 @@ $: << File.dirname(__FILE__)
 require 'lib/process_info'
 
 def render_proc_info
-  contents = File.open("lib/views/process_info.slim", "rb").read
+  contents = File.open("lib/views/_process_table.slim", "rb").read
   template = Slim::Template.new { contents }
   procs = ProcMon::ProcessInfo.all
   template.render procs, {procs:procs}
